d58dab1a45b91b488f9043ab6de879af9f831520
Jiajun Xu <131632195+JiajunXu2@users.noreply.github.com>
2023-05-01 14:48:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ client = commands.Bot(command_prefix = ".")
 async def on_ready():
   print('Jia bot is logged in as {0.user}'.format(client))
   #this is called when the bot is ready to start being used
+  
+@client.event
+async def on_message(message):
+  if message.content.startswith('$hello'):
+    if message.author.id == 207251811504095232:
+      await message.channel.send("hi jun!")
+    else:
+      await message.channel.send("hello")
+  if message.content.startswith('$num'):
+    value = random.randint(0, 100)
+    await message.channel.send("random value is " + str(value))
+  await client.process_commands(message)
+
