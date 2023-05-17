cbbf61dec1157290409695d80a7cac0987df9d09
Jiajun Xu <131632195+JiajunXu2@users.noreply.github.com>
2023-05-16 20:06:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,27 @@ async def on_message(message):
     value = random.randint(0, 100)
     await message.channel.send("random value is " + str(value))
   await client.process_commands(message)
-
+ 
+@client.command(aliases = ["8ball"])
+async def _8ball(ctx, *, question):
+  responses =  ["It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes - definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+  await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
